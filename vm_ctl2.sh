063fd25bb136d4ab50b49140c8ca2d390ea51ebc
iD3sk <fiqhideski@gmail.com>
2026-09-19 20:58:46 +0700

    feat(bagian 1): show RAM and vCPU info

--- vm_ctl2.sh
+++ vm_ctl2.sh
@@ -1,9 +1,33 @@
+info() {
+    echo "==================================="
+    echo "     TUGAS 1 OS - KELOMPOK B08     "
+    echo "==================================="	
+    
+    if VBoxManage list vms | awk -F'"' '{print $2}' | grep -Fxq "$2"; then
+        vm = "$2"
+        echo "VM Ditemukan: $vm"
+        
+                
+    else
+        echo "VM tidak ditemukan"
+        return
+
+    echo "Memindai RAM dan vCPU"
+    memory = $(VBoxManage showvminfo "$2" --machinereadable | grep '^memory=' | cut -d'=' -f2)
+    vcpu   = $(VBoxManage showvminfo "$2" --machinereadable | grep '^cpu=' | cut -d'=' -f2)
+
+    echo "RAM: $memory"
+    echo "vCPU: $vcpu"
+}
+
+
 case "$1" in
     list)
         list_vms
         ;;
     info)
-        ;;
+        info
+	;;
     start)
         ;;
     snapshot)
